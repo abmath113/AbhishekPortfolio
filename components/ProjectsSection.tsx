3044501042931d87166642faf8627b823060886d
Abhishek Thorat <83878982+abmath113@users.noreply.github.com>
2023-08-13 16:52:13 +0530

    Improved Project description layout

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,45 +1,42 @@
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
-import SlideUp from "./SlideUp"
-import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SlideUp from "./SlideUp";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
 
 const projects = [
   {
     name: "Anuvadak: ISL Translation",
-    description:
-      "An Android app, to translate indian sign language to text in real time , and also provide signs for common",
+    points: [
+      "Developed an Android app to translate Indian Sign Language (ISL) to text in real time.",
+      "Implemented palm detection and sign identification using Tensorflow, Mediapipe, and OpenCV.",
+      "Utilized various deep learning algorithms, including LSTM, to train the model on over 3000 images.",
+    ],
     image: "/thankfulthoughts.png",
     github: "https://github.com/abmath113/Anuvadak",
     link: "https://github.com/abmath113/Anuvadak",
   },
   {
     name: "Used Books Marketplace App",
-    description: "Created an Android app for college students where they can sell and buy used textbooks",
+    points: [
+      "Created an Android app for college students to buy and sell used textbooks.",
+      "Provided a platform for easy exchange of textbooks within the college community.",
+    ],
     image: "/booksapp.jpg",
     github: "https://github.com/abmath113/Android-Marketplace-App",
     link: "https://github.com/abmath113/Android-Marketplace-App",
   },
-  // {
-  //   name: "Kator Family Photos",
-  //   description:
-  //     "Kator Family Photos is a photos and video digitization service in the LA area.",
-  //   image: "/familyphotos.png",
-  //   github: "https://github.com/hqasmei/katorfamilyphotos",
-  //   link: "https://katorfamilyphotos.com/",
-  // },
-]
+];
 
 const ProjectsSection = () => {
   return (
     <section id="projects">
-   <h1 className="my-10 text-center font-bold text-4xl">
-  <span className="hover:scale-110 inline-block transition-transform">
-    Projects
-  </span>
-  <hr className="w-6 h-1 mx-auto my-4 bg-blue-500 border-0 rounded"></hr>
-</h1>
-
+      <h1 className="my-10 text-center font-bold text-4xl">
+        <span className="hover:scale-110 inline-block transition-transform">
+          Projects
+        </span>
+        <hr className="w-6 h-1 mx-auto my-4 bg-blue-500 border-0 rounded"></hr>
+      </h1>
 
       <div className="flex flex-col space-y-28">
         {projects.map((project, idx) => {
@@ -60,9 +57,13 @@ const ProjectsSection = () => {
                   </div>
                   <div className="mt-8 md:w-1/2">
                     <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
-                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                      {project.description}
-                    </p>
+                    <ul className="list-disc pl-6">
+                      {project.points.map((point, index) => (
+                        <li key={index} className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                          {point}
+                        </li>
+                      ))}
+                    </ul>
                     <div className="flex flex-row align-bottom space-x-4">
                       <Link href={project.github} target="_blank">
                         <BsGithub
@@ -81,12 +82,11 @@ const ProjectsSection = () => {
                 </div>
               </SlideUp>
             </div>
-          )
+          );
         })}
-        
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProjectsSection
+export default ProjectsSection;
