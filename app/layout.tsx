3ea433055b8f7a50c3775f0ab427e566504a0cb1
Abhishek Thorat <83878982+abmath113@users.noreply.github.com>
2024-10-20 20:51:34 +0530

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,27 @@
-"use client"
-import "../styles/globals.css"
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
-import { ThemeProvider } from "next-themes"
+"use client";
+import "../styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body className="dark:bg-black">
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
           {children}
+          <Analytics />
           <Footer />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
+
